b581f9a85ed570253b2b83b1737e28c421a6779e
Jérémy Marabel <jeremy.marabel@gmail.com>
2016-11-02 20:19:11 +0100

    Ajout du menu

---
+++ IconGame/app/src/main/java/com/iut/german_marabel/icongame/GameActivity.java
@@ -0,0 +1,13 @@
+package com.iut.german_marabel.icongame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
