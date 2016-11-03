f8a82ccac394f5c89e118b3749c5ed49ca1043b9
Jérémy Marabel <jeremy.marabel@gmail.com>
2016-11-03 22:26:40 +0100

    Ajout du menu de préférences
    L'utilisateur peut rentrer son nom, choisir le nombre d'icones et
    activer le mode difficile. Ces valeurs sont sauvegardées même si
    l'application est fermée.

--- IconGame/app/src/main/java/com/iut/german_marabel/icongame/MenuActivity.java
+++ IconGame/app/src/main/java/com/iut/german_marabel/icongame/MenuActivity.java
@@ -2,10 +2,13 @@ package com.iut.german_marabel.icongame;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -34,7 +37,7 @@ public class MenuActivity extends AppCompatActivity {
         });
         option.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent myIntent = new Intent(MenuActivity.this, GameActivity.class);
+                Intent myIntent = new Intent(MenuActivity.this, MyPreferenceActivity.class);
                 MenuActivity.this.startActivity(myIntent);
             }
         });
@@ -44,6 +47,17 @@ public class MenuActivity extends AppCompatActivity {
                 MenuActivity.this.startActivity(myIntent);
             }
         });
+
+        //recupération des data de setting exemple
+        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String strUserName = SP.getString("username","");
+        boolean bHardMode = SP.getBoolean("difficulty",false);
+        String NbIcon = SP.getString("nbIcons","1");
+
+        Log.d("",strUserName);
+        Log.d("",NbIcon);
+
+
 
     }
 
---
+++ IconGame/app/src/main/java/com/iut/german_marabel/icongame/MyPreferenceActivity.java
@@ -0,0 +1,25 @@
+package com.iut.german_marabel.icongame;
+
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPreferenceActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
