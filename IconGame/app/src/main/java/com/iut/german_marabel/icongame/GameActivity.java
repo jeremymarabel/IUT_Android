68c0b7868e956665ca03caebbf6099d9de12dc15
Arnaud german <arnaud.german@gmail.com>
2016-11-03 22:18:47 +0100

    génération de coordonnées
    génération aléatoire de coordonées

--- IconGame/app/src/main/java/com/iut/german_marabel/icongame/GameActivity.java
+++ IconGame/app/src/main/java/com/iut/german_marabel/icongame/GameActivity.java
@@ -1,12 +1,15 @@
 package com.iut.german_marabel.icongame;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Debug;
 import android.os.Handler;
 import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,10 +19,13 @@ import android.widget.TextView;
 
 import java.io.Console;
 import java.util.HashMap;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
-    final HashMap<Integer,View> images = new HashMap<Integer,View>();
+    final HashMap<Integer,ImageView> images = new HashMap<Integer,ImageView>();
+
+    int nbIcons = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +37,21 @@ public class GameActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
 
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.relLayout);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
 
-        View v1 = new ImageView(this);
-        v1.setMinimumWidth(100);
-        v1.setMinimumHeight(100);
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        size.y = size.y - statusBarHeight*2;
+
+        ImageView v1 = getNewIcon(this,rl,100,size);
+
         v1.setBackgroundColor(Color.BLUE);
         images.put(0,v1);
 
@@ -49,25 +65,56 @@ public class GameActivity extends AppCompatActivity {
 
 
 
-        RelativeLayout rl = (RelativeLayout) findViewById(R.id.relLayout);
+
 
         rl.addView(v1);
 
 
-        final Handler handler = new Handler();
-        handler.post(new Runnable(){
-            @Override
-            public void run() {
-                View v2 = images.get(0);
-                v2.setLeft(v2.getLeft()+6);
-                v2.setRight(v2.getRight()+6);
-                handler.postDelayed(this,1);
-            }
-        });
+//        final Handler handler = new Handler();
+//        handler.post(new Runnable(){
+//            @Override
+//            public void run() {
+//                View v2 = images.get(0);
+//                v2.setLeft(v2.getLeft()+6);
+//                v2.setRight(v2.getRight()+6);
+//                handler.postDelayed(this,1);
+//            }
+//        });
 
     }
 
-
+    public ImageView getNewIcon(Context c, RelativeLayout rl, int size, Point screenSize)
+    {
+        ImageView v = new ImageView(c);
+        int maxX = screenSize.x-size;
+        int maxY = screenSize.y-size;
+        boolean positionNotFound = true;
+        Random r = new Random();
+        int x = 0;
+        int y = 0;
+        while(positionNotFound)
+        {
+            positionNotFound = false;
+            x = r.nextInt(maxX);
+            y = r.nextInt(maxY);
+            for (int i = 0 ; i < images.size() ; i++)
+            {
+                ImageView oneImage = images.get(i);
+                if(oneImage.getX() - x < size || oneImage.getY() - y < size)
+                {
+                    positionNotFound = true;
+                    break;
+                }
+            }
+        }
+        v.setX(x);
+        v.setY(y);
+        v.setMinimumWidth(size);
+        v.setMaxWidth(size);
+        v.setMinimumHeight(size);
+        v.setMaxHeight(size);
+        return v;
+    }
 
 
 }
